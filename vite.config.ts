29e234012b4a99f3eb7237569180e9e64ae0d1b3
mbmkhize22 <mbmkhize22@gmail.com>
2025-11-02 14:13:25 +0200

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fuelgo/',  // 👈 this must match your repo name
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  },
-});
+})
