b3057e45caa77ddad30adeb3aaa5563fdd3dbfba
Nhlanhla Mkhize <mbmkhize22@gmail.com>
2025-11-02 17:59:05 +0200

    workflows updated

--- src/pages/client/ClientHome.jsx
+++ src/pages/client/ClientHome.jsx
@@ -113,7 +113,7 @@ export default function ClientHome() {
       {/* Floating Request Button */}
       <div className="floating-btn-container">
         <Link to="/client/request" className="floating-btn">
-          Request Fuel / Gas Test
+          Request Fuel / Gas Test2
         </Link>
       </div>
 
